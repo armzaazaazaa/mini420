cc6de52e792f984ed34618e3e328e52173e56fd7
CHoosa <choocharmon@gmail.com>
2015-12-09 12:27:55 +0700

    Update TestCase.php

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,11 +2,7 @@
 
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
-    /**
-     * The base URL to use while testing the application.
-     *
-     * @var string
-     */
+   
     protected $baseUrl = 'http://localhost';
 
     /**
